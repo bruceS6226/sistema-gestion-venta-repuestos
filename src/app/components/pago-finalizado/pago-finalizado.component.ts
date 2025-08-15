881de552a86e868dec50a50a0ee38194fe9754b8
Bruce Soto <bruce.soto@epn.edu.ec>
2025-08-15 12:21:47 -0500

    Actualizar carrito

--- src/app/components/pago-finalizado/pago-finalizado.component.ts
+++ src/app/components/pago-finalizado/pago-finalizado.component.ts
@@ -17,10 +17,41 @@ export class PagoFinalizadoComponent implements OnInit{
 
   constructor(private _errorService: ErrorService,
     private _actualizarComponentService: ActualizarComponentService,) {
-
+    this._actualizarComponentService.actualizarDetalleCarrito$.subscribe({
+      next: () => {
+        this.ngOnInit()
+      },
+    });
+    this._actualizarComponentService.actualizarHeader$.subscribe(() => {
+      this.loadCarritoFromLocalStorage();
+    });
   }
 
+  private _cantidadRepuestosSubject = new BehaviorSubject<number>(0);
+  public cantidadRepuestosSeleccionadosParaComprar$: Observable<number> = this._cantidadRepuestosSubject.asObservable();
+private loadCarritoFromLocalStorage(): void {
+    const repuestosJson = localStorage.getItem('repuestosSeleccionadosParaCompra');
+    if (repuestosJson) {
+      try {
+        this.repuestosSeleccionadosParaCompra = JSON.parse(repuestosJson);
+        this.cantidadRepuestosSeleccionadosParaComprar = this.repuestosSeleccionadosParaCompra.length;
+        this._cantidadRepuestosSubject.next(this.cantidadRepuestosSeleccionadosParaComprar);
+      } catch (error) {
+        console.error('Error al analizar los datos del localStorage:', error);
+        this.repuestosSeleccionadosParaCompra = [];
+        this.cantidadRepuestosSeleccionadosParaComprar = 0;
+        this._cantidadRepuestosSubject.next(0);
+      }
+    } else {
+      this.repuestosSeleccionadosParaCompra = [];
+      this.cantidadRepuestosSeleccionadosParaComprar = 0;
+      this._cantidadRepuestosSubject.next(0);
+    }
+  }
   ngOnInit(): void {
     localStorage.removeItem('repuestosSeleccionadosParaCompra');
+    this.loadCarritoFromLocalStorage();
+    this._actualizarComponentService.notificarDetalleCarrito();
+    this._actualizarComponentService.notificarHeader();
   }
 }
